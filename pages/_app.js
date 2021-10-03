3723f84a8f7f8f72774d80ca20dfde15613d69a4
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-10-03 15:56:41 +0530

    Initial Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import { Provider } from 'react-redux'
 import 'tailwindcss/tailwind.css'
+import '../style/global.css'
+
+import store from '../store/store'
+import FirebaseAuthState from '../config/FirebaseAuthState'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Provider store={store}>
+        <FirebaseAuthState>
+          <Component {...pageProps} />
+        </FirebaseAuthState>
+      </Provider>
+    </>
+  )
 }
 
 export default MyApp
