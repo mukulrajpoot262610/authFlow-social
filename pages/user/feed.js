87d5c96670468a4f8c295783162faf3991a14786
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-10-14 17:17:26 +0530

    Hotfix

--- pages/user/feed.js
+++ pages/user/feed.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import withAuth from '../../utils/withAuth'
+// import withAuth from '../../utils/withAuth'
 
 const feed = () => {
     return (
@@ -10,9 +10,30 @@ const feed = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h1>This is protected page</h1>
+            <div className="container w-full flex mt-24">
+                <div className="w-3/12 h-screen bg-green-500"></div>
+                <div className="w-6/12 h-screen bg-red-500"></div>
+                <div className="w-3/12 h-screen p-4">
+                    <div className="h-1/2 overflow-auto">
+                        <h1 className="stat-title uppercase font-bold my-2">Requests</h1>
+                        <div className="w-full shadow-xl stats my-2">
+                            <div className="stat flex flex-col">
+                                <div className="flex justify-between items-center">
+                                    <img src="/logo.png" alt="Avatar Tailwind CSS Component" className="h-16 w-16 mask mask-squircle" />
+                                    <div className="stat-title">Tasks done</div>
+                                </div>
+                                <div className="mt-3 w-full flex">
+                                    <button className="btn btn-primary w-1/2" >Accept</button>
+                                    <button className="btn btn-secondary w-1/2">Decline</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
 
-export default withAuth(feed)
+export default feed
